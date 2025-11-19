aeaf67e10a2202cb0a2be5203a7218b23fc4e90b
MR-B-Star <adityabintang197@gmail.com>
2025-11-19 14:44:46 +0700

    Add files via upload

--- TrashTruck.py
+++ TrashTruck.py
@@ -22,11 +22,93 @@ Hijau_tua = (0.1, 0.4, 0.1)
 Hijau_tua2 = (0.05, 0.3, 0.05)
 Hijau_muda = (0.4, 0.6, 0.4)
 Hijau_muda2 = (0.6, 0.8, 0.6)
+Kuning = (1.0, 1.0, 0.0)
+Merah = (0.8, 0.1, 0.1)
+Abu_Tua = (0.3, 0.3, 0.3)
+Abu_Tua2 = (0.4, 0.4, 0.4)
+Abu_Tua3 = (0.2, 0.2, 0.2)
+Abu_Muda = (0.7, 0.7, 0.7)
+
+
+
+# roda belakang truk sisi kanan2
+ctx.set_source_rgb(*Abu_Tua3)
+ctx.arc(920, 520, 80, 60, 50 * math.pi)  # Roda belakang
+ctx.fill()
+
+# roda belakang truk sisi kanan
+ctx.set_source_rgb(*Abu_Tua)
+ctx.arc(880, 520, 80, 60, 50 * math.pi)  # Roda belakang
+ctx.fill()
+
+# vleg roda belakang truk
+ctx.set_source_rgb(*Abu_Muda)
+ctx.arc(880, 520, 50, 50, 50 * math.pi)  # Roda belakang
+ctx.fill()
+
+# kotak bawah truk
+ctx.set_source_rgb(0.3, 0.3, 0.3)
+ctx.rectangle(200, 450, 390, 20)  # Badan truk
+ctx.fill()
+
+# kotak bawah truk
+ctx.set_source_rgb(0, 0, 0)
+ctx.rectangle(700, 550, 390, 20)  # Badan truk
+ctx.fill()
+
+# kotak bawah truk2
+ctx.set_source_rgb(0.6, 0.6, 0.6)
+ctx.rectangle(870, 550, 160, 25)  # Badan truk
+ctx.fill()
+
+# kotak bawah truk3
+ctx.set_source_rgb(0.8, 0.8, 0.8)
+ctx.rectangle(880, 555, 140, 15)  # Badan truk
+ctx.fill()
 
 # truck body
 ctx.set_source_rgb(*Hijau2)  # Hijau
-ctx.rectangle(400, 170, 600, 280)  # Badan truk
+ctx.rectangle(200, 170, 800, 280)  # Badan truk
 ctx.fill()
+
+# roda belakang truk gradasi2
+ctx.set_source_rgb(*Abu_Tua3)
+ctx.arc(660, 520, 80, 60, 50 * math.pi)  # Roda belakang
+ctx.fill()
+
+# roda belakang truk gradasi1
+ctx.set_source_rgb(*Abu_Tua)
+ctx.arc(630, 520, 80, 60, 50 * math.pi)  # Roda belakang
+ctx.fill()
+
+# roda belakang truk (kotak)
+ctx.set_source_rgb(*Abu_Tua)
+ctx.rectangle(600, 505, 30, 95)  # Roda belakang
+ctx.fill()
+
+#roda belakang truk
+ctx.set_source_rgb(*Abu_Tua2)
+ctx.arc(600, 520, 80, 50, 50 * math.pi)  # Roda belakang
+ctx.fill()
+
+# vleg roda belakang truk
+ctx.set_source_rgb(*Abu_Muda)
+ctx.arc(600, 520, 50, 50, 50 * math.pi)  # Roda belakang
+ctx.fill()
+
+# garis dalam vleg roda belakang truk
+ctx.set_source_rgb(1, 1, 1)
+for i in range(8):
+    angle = i * (math.pi / 4)
+    x_start = 600 + 0 * math.cos(angle)
+    y_start = 520 + 0 * math.sin(angle)
+    x_end = 600 + 50 * math.cos(angle)
+    y_end = 520 + 50 * math.sin(angle)
+    ctx.set_line_width(2)
+    ctx.move_to(x_start, y_start)
+    ctx.line_to(x_end, y_end)
+    ctx.stroke()
+
 
 # segitiga samping truk
 ctx.set_source_rgb(*Hijau2)
@@ -44,6 +126,49 @@ ctx.line_to(1100, 550)   # Puncak atas
 ctx.line_to(750, 550)   # Puncak atas
 ctx.close_path()
 ctx.fill()
+
+# trapesium gradasi truk
+ctx.set_source_rgb(*Hijau)
+ctx.move_to(720, 450)   # Bawah kiri
+ctx.line_to(805, 480)   # Bawah kanan
+ctx.line_to(805, 550)   # Puncak atas kanan
+ctx.line_to(730, 550)   # Puncak atas kiri
+ctx.close_path()
+ctx.fill()
+
+# trapesium gradasi truk2
+ctx.set_source_rgb(*Hijau)
+ctx.move_to(580, 170)   # Bawah kiri
+ctx.line_to(710, 170)   # Bawah kanan
+ctx.line_to(805, 480)   # Puncak atas kanan
+ctx.line_to(730, 550)   # Puncak atas kiri
+ctx.close_path()
+ctx.fill()
+
+# kotak gradasi truk
+ctx.set_source_rgb(*Hijau)
+ctx.rectangle(280, 170, 430, 280)  # Badan truk
+ctx.fill()
+
+# trapesium gradasi truk3
+ctx.set_source_rgb(*Hijau_tua)
+ctx.move_to(600, 190)   # Bawah kiri
+ctx.line_to(690, 190)   # Bawah kanan
+ctx.line_to(750, 395)   # Puncak atas kanan
+ctx.line_to(600, 395)   # Puncak atas kiri
+ctx.close_path()
+ctx.fill()
+
+# kotak gradasi truk4
+ctx.set_source_rgb(*Hijau_tua)
+ctx.rectangle(460, 190, 110, 205)  # Badan truk
+ctx.fill()
+
+# kotak gradasi truk5
+ctx.set_source_rgb(*Hijau_tua)
+ctx.rectangle(320, 190, 110, 205)  # Badan truk
+ctx.fill()
+
 
 # trapesium atas truk
 ctx.set_source_rgb(*Hijau_muda)
@@ -74,142 +199,114 @@ ctx.fill()
 
 # trapesium-trapesium kecil di badan truk
 ctx.set_source_rgb(*Hijau_tua2)
-i in range(5):
-    x_offset = 410 + i * 115
-    ctx.move_to(x_offset, 200)   # Bawah kiri
-    ctx.line_to(x_offset + 80, 200)   # Bawah kanan
-    ctx.line_to(x_offset + 80, 300)   # Puncak atas
-    ctx.line_to(x_offset, 300)   # Puncak atas
+for i in range(2):
+    x_offset = 755 + i * 125
+    ctx.move_to(x_offset, 250)   # Bawah kiri
+    ctx.line_to(x_offset + 80, 250)   # Bawah kanan
+    ctx.line_to(x_offset + 102, 310)   # Puncak atas kanan
+    ctx.line_to(x_offset + 20 , 310)   # Puncak atas kiri
     ctx.close_path()
     ctx.fill()
 
-# #==============================
-# #   dinding sisi ruang mesin
-# #==============================
+ctx.set_source_rgb(*Hijau_tua2)
+for i in range(2):
+    x_offset = 353 + i * 128
+    ctx.move_to(x_offset + 505, 320)   # Bawah kiri
+    ctx.line_to(x_offset + 425, 320)   # Bawah kanan
+    ctx.line_to(x_offset + 445, 380)   # Puncak atas kiri
+    ctx.line_to(x_offset + 525, 380)   # Puncak atas kanan
+    ctx.close_path()
+    ctx.fill()
 
-# # Dinding belakang (area terang atas)
-# ctx.set_source_rgb(*gray_light)
-# ctx.rectangle(53, 0, 1119, 268)
-# ctx.fill()
+ctx.set_source_rgb(*Hijau_tua2)
+for i in range(2):
+    x_offset = 480 + i * 130
+    ctx.move_to(x_offset + 400, 390)   # Bawah kiri
+    ctx.line_to(x_offset + 320, 390)   # Bawah kanan
+    ctx.line_to(x_offset + 340, 450)   # Puncak atas kiri
+    ctx.line_to(x_offset + 420, 450)   # Puncak atas kanan
+    ctx.close_path()
+    ctx.fill()
 
-# # dinding belakang (area terang sisi kiri)
-# ctx.set_source_rgb(*gray_medium)
-# ctx.rectangle(0, 0, 53, 268)
-# ctx.fill()
+# tiga kotak gradasi merah putih kesamping truk
+ctx.set_source_rgb(*Merah)
+for i in range(3):
+    x_offset = 740 + i * 35
+    ctx.rectangle(x_offset, 190, 20, 10)
+    ctx.fill()
 
-# # dinding belakang (segitiga atas kiri bawah)
-# ctx.set_source_rgb(*gray_medium)
-# ctx.move_to(0, 440)   # Bawah kiri
-# ctx.line_to(0, 265)   # Bawah kanan
-# ctx.line_to(53, 260)   # Puncak atas
-# ctx.close_path()
-# ctx.fill()
+ctx.set_source_rgb(1, 1, 1)
+for i in range(2):
+    x_offset = 760 + i * 35
+    ctx.rectangle(x_offset, 190, 15, 10)
+    ctx.fill()
 
-# # dinding belakang (area terang sisi kanan)
-# ctx.set_source_rgb(*gray_medium)
-# ctx.rectangle(1172, 0, 54, 268)
-# ctx.fill()
+ctx.set_source_rgb(*Merah)
+for i in range(3):
+    x_offset = 900 + i * 35
+    ctx.rectangle(x_offset, 190, 20, 10)
+    ctx.fill()
 
-# # dinding belakang terang (segitiga atas kanan atas)
-# ctx.set_source_rgb(*gray_medium)
-# ctx.move_to(1172, 260)   # Bawah kiri
-# ctx.line_to(1231, 440)   # Bawah kanan
-# ctx.line_to(1231, 260)   # Puncak atas
-# ctx.close_path()
-# ctx.fill()
+ctx.set_source_rgb(1, 1, 1)
+for i in range(2):
+    x_offset = 920 + i * 35
+    ctx.rectangle(x_offset, 190, 15, 10)
+    ctx.fill()
 
-# # Dinding belakang (area gelap bawah)
-# ctx.set_source_rgb(*gray_dark)
-# ctx.rectangle(53, 260, 1119, 306)
-# ctx.fill()
+# kotak lampu truk kiri
+ctx.set_source_rgb(*Hijau_tua2)
+ctx.rectangle(825, 470, 60, 70)
+ctx.fill()
 
-# # Dinding belakang (area gelap sisi kiri)
-# ctx.set_source_rgb(*gray_dark_sha)
-# ctx.rectangle(0, 440, 53, 260)
-# ctx.fill()
+# kotak lampu truk kiri (kuning)
+ctx.set_source_rgb(*Kuning)
+ctx.rectangle(835, 480, 40, 20)
+ctx.fill()
 
-# # dinding belakang (segitiga atas kiri atas)
-# ctx.set_source_rgb(*gray_dark_sha)
-# ctx.move_to(0, 440)   # Bawah kiri
-# ctx.line_to(53, 440)   # Bawah kanan
-# ctx.line_to(53, 260)   # Puncak atas
-# ctx.close_path()
-# ctx.fill()
+# kotak lampu truk kiri (merah)
+ctx.set_source_rgb(*Merah)
+ctx.rectangle(835, 510, 40, 20)
+ctx.fill()
 
-# # dinding belakang (segitiga atas kiri bawah)
-# ctx.set_source_rgb(*gray_dark_sha)
-# ctx.move_to(0, 725)   # Bawah kiri
-# ctx.line_to(0, 505)   # Bawah kanan
-# ctx.line_to(54, 505)   # Puncak atas
-# ctx.close_path()
-# ctx.fill()
+# kotak lampu truk kanan
+ctx.set_source_rgb(*Hijau_tua2)
+ctx.rectangle(1025, 470, 60, 70)
+ctx.fill()
 
-# # Dinding belakang (area gelap sisi kanan)
-# ctx.set_source_rgb(*gray_dark_sha)
-# ctx.rectangle(1172, 420, 54, 309)
-# ctx.fill()
+# kotak lampu truk kanan (kuning)
+ctx.set_source_rgb(*Kuning)
+ctx.rectangle(1035, 480, 40, 20)
+ctx.fill()
 
-# # dinding belakang (segitiga atas kanan atas)
-# ctx.set_source_rgb(*gray_dark_sha)
-# ctx.move_to(1172, 440)   # Bawah kiri
-# ctx.line_to(1172, 260)   # Bawah kanan
-# ctx.line_to(1231, 440)   # Puncak atas
-# ctx.close_path()
-# ctx.fill()
+# kotak lampu truk kanan (merah)
+ctx.set_source_rgb(*Merah)
+ctx.rectangle(1035, 510, 40, 20)
+ctx.fill()
 
+# Kotak sebelah kotak lampu truk kiri
+ctx.set_source_rgb(*Hijau_tua2)
+ctx.rectangle(895, 460, 6, 125)
+ctx.fill()
 
-# #==============================
-# #     lantai ruang mesin
-# #==============================
+# garis untuk tangga truk belakang
+ctx.set_source_rgb(*Hijau_tua2)
+for i in range(8):
+    y_offset = 460 + i * 15
+    ctx.rectangle(890, y_offset, 115, 5)
+    ctx.fill()
 
-# # Lantai bawah (terang)
-# ctx.set_source_rgb(*gray_medium)
-# ctx.move_to(54, 505)
-# ctx.line_to(1172, 505)
-# ctx.line_to(1225, 689)
-# ctx.line_to(0, 689)
-# ctx.fill()
+# Kotak sebelah kotak lampu truk kanan
+ctx.set_source_rgb(*Hijau_tua2)
+ctx.rectangle(1005, 460, 6, 125)
+ctx.fill()
 
-# # Radiator
-# radiator_x = 77
-# radiator_y = 24
-# radiator_width = 155
-# radiator_height = 90
+# garis gradasi tangga truck belakang
+ctx.set_source_rgb(*Hijau_tua)
+for i in range(8):
+    y_offset = 465 + i * 15
+    ctx.rectangle(895, y_offset, 120, 5)
+    ctx.fill()
 
-# # Frame radiator
-# ctx.set_source_rgb(0, 0, 0)
-# ctx.set_line_width(3)
-# ctx.rectangle(radiator_x, radiator_y, radiator_width, radiator_height)
-# ctx.stroke()
-
-# # Garis horizontal atas radiator
-# ctx.set_line_width(2)
-# ctx.move_to(radiator_x, radiator_y + 15)
-# ctx.line_to(radiator_x + radiator_width, radiator_y + 15)
-# ctx.stroke()
-
-# # Panel vertikal radiator
-# num_panels = 6
-# panel_width = radiator_width / num_panels
-
-# for i in range(num_panels):
-#     x = radiator_x + i * panel_width + panel_width * 0.2
-#     panel_w = panel_width * 0.6
-    
-#     # Panel utama
-#     ctx.rectangle(x, radiator_y + 18, panel_w, radiator_height - 20)
-#     ctx.stroke()
-
-# # Meja kayu di pojok kiri bawah
-# ctx.set_source_rgb(*wood_color)
-
-# # Bentuk trapesium meja (perspektif)
-# ctx.move_to(40, 580)
-# ctx.line_to(375, 580)
-# ctx.line_to(360, 700)
-# ctx.line_to(0, 700)
-# ctx.close_path()
-# ctx.fill()
 
 
 # Simpan
